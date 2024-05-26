1dee6e8e41d8c81ac418de5cf5c7720d95c49a58
AMB-Dangha <ha.dang@amoeba.group>
2024-05-26 18:32:52 +0700

    update get DB info from env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,21 +6,30 @@ import { Person } from './persons/person.entity';
 import { PersonsModule } from './persons/person.module';
 import { AuthModule } from './users/auth.module';
 import { User } from './users/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '123456',
-    database: 'postgres',
-    entities: [Person, User],
-    synchronize: true,
-    autoLoadEntities: true,
-  }),
-  AuthModule,
-  PersonsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [Person, User],
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+    }),
+    AuthModule,
+    PersonsModule],
   controllers: [AppController],
   providers: [AppService],
   exports: [TypeOrmModule]
