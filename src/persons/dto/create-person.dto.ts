6d90c02d25188b47b588a7cca5be2c87cc96cdea
AMB-Dangha <160574874+AMB-Dangha@users.noreply.github.com>
2024-05-28 10:15:19 +0700

    add validate for create person

--- src/persons/dto/create-person.dto.ts
+++ src/persons/dto/create-person.dto.ts
@@ -1,11 +1,34 @@
-export interface CreatePersonDto {
-    id: number;
-    firstName: string;
-    lastName: string;
-    dateOfBirth: Date;
-    gender: string;
-    address: string;
-    email: string;
-    phoneNumber: string;
-    relationship: string;
+import { IsString, IsNotEmpty, IsDateString } from 'class-validator';
+
+export class CreatePersonDto {
+  @IsString({ message: 'First name must be a string' })
+  @IsNotEmpty({ message: 'First name is required' })
+  firstName: string;
+
+  @IsString({ message: 'Last name must be a string' })
+  @IsNotEmpty({ message: 'Last name is required' })
+  lastName: string;
+
+  @IsDateString({}, { message: 'Date of birth must be a valid date' })
+  dateOfBirth: string;
+
+  @IsString({ message: 'Gender must be a string' })
+//   @IsNotEmpty({ message: 'Gender is required' })
+  gender: string;
+
+  @IsString({ message: 'Address must be a string' })
+//   @IsNotEmpty({ message: 'Address is required' })
+  address: string;
+
+  @IsString({ message: 'Email must be a string' })
+//   @IsNotEmpty({ message: 'Email is required' })
+  email: string;
+
+  @IsString({ message: 'Phone number must be a string' })
+//   @IsNotEmpty({ message: 'Phone number is required' })
+  phoneNumber: string;
+
+  @IsString({ message: 'Relationship must be a string' })
+//   @IsNotEmpty({ message: 'Relationship is required' })
+  relationship: string;
 }
