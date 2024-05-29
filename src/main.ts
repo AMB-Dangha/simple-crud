09c7d8f92f62a33d441ecf1406f076f135d70eee
AMB-Dangha <160574874+AMB-Dangha@users.noreply.github.com>
2024-05-29 11:23:06 +0700

    add transform response data interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './http-exception.filter';
 import { ValidationPipe } from './validation.pipe';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/transform.interceptor.ts
@@ -0,0 +1,33 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor, HttpException } from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<T, any> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next
+      .handle()
+      .pipe(
+        map(data => ({
+          statusCode: context.switchToHttp().getResponse().statusCode,
+          data,
+          msg: 'Success',
+          error: null,
+        })),
+        // catchError((err) => {
+        //   let error = err;
+        //   let message = 'An error occurred';
+        //   if (err instanceof HttpException) {
+        //     error = err.getResponse();
+        //     message = err.message;
+        //   }
+        //   return throwError(() => ({
+        //     statusCode: context.switchToHttp().getResponse().statusCode,
+        //     data: null,
+        //     msg: message,
+        //     error,
+        //   }));
+        // }),
+      );
+  }
+}
